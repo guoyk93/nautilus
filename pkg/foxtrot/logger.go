0128e84491d91329624126bdb62f112b8acb0114
Guo Y.K <hi@guoyk.net>
2020-01-30 20:28:17 +0800

    update UI

--- pkg/foxtrot/logger.go
+++ pkg/foxtrot/logger.go
@@ -5,6 +5,7 @@ import "github.com/labstack/echo/v4"
 func NewLogger() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) (err error) {
+			//TODO: integrate with zerolog
 			return next(c)
 		}
 	}
